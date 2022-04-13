c8560bf37f8869172b7e4b050a0c602a8262b2f4
eduar <eduardvitko>
2022-04-13 13:33:49 +0300

    commit directory

---
+++ src/main/java/ru/home/taskswebservise/dao/UserDao.java
@@ -0,0 +1,12 @@
+//package home.dao;
+//import home.model.User;
+//import java.sql.SQLException;
+//import java.util.Optional;
+//
+//public interface UserDao  {
+//    int insertUser(User user) throws SQLException;
+//
+//    Optional<User> findByUsername(String username) throws SQLException;
+//
+//    Optional<User> findByEmail(String email) throws SQLException;
+//}
